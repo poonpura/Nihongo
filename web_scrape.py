02df4ebbe32383dbf84c48424fc4198e73677d4f
Pura Peetathawatchai <pp448@cornell.edu>
2020-03-09 23:26:07 -0400

    added recursive tree searching

--- web_scrape.py
+++ web_scrape.py
@@ -16,6 +16,7 @@ Author: Pura Peetathawatchai
 import string
 from selenium import webdriver
 import freq_dict
+import copy
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -23,7 +24,8 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 # Set path name to be path to webdriver
-browser = webdriver.Chrome("/Users/Pura/Documents/chromedriver", chrome_options=options)
+def init_browser():
+    return webdriver.Chrome("/Users/Pura/Documents/chromedriver", chrome_options=options)
 
 """
 Removes all Roman characters and punctuation from ```text``` so that only
@@ -43,6 +45,7 @@ def _deromanize(text):
 Returns the HTML string of the web page given by ```URL```.
 """
 def _get_html(url):
+    browser = init_browser()
     browser.get(url)
     html = browser.page_source
     browser.quit()
@@ -68,3 +71,41 @@ Returns the frequency ranking of the textual contents of ```URL```.
 """
 def rank(url):
     return _on_url(lambda x: freq_dict.f_rank(x), url)
+
+def _add(d1, d2):
+    d= copy.deepcopy(d1)
+    for k in d2.keys():
+        if (k in d.keys()):
+            d[k]= d[k] + d2[k]
+        else:
+            d[k]= d2[k]
+    return d
+
+def tree_dict(url):
+    def tree_dict_acc(url, acc):
+        if url in acc:
+            return {}, acc
+
+        browser = init_browser()
+        browser.get(url)
+        r_url = browser.current_url
+        elems = set(browser.find_elements_by_xpath("//*[contains(@href,'"+r_url+"')]"))
+        acc= acc + [r_url]
+
+        link_lst= []
+        for elem in elems:
+            link_lst.append(elem.get_attribute("href"))
+        links = set(link_lst)
+
+        dict= freq_dict.f(_deromanize(browser.page_source))
+        browser.quit()
+        for link in links:
+            xtr, acc= tree_dict_acc(link, acc)
+            dict= _add(dict, xtr)
+
+        return dict, acc
+
+    dict, _ = tree_dict_acc(url, [])
+    return dict
+
+print(tree_dict("https://www.nintendo.co.jp/"))
